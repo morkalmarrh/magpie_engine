44e9bc347c179ece838e83131f594f1c9d128918
morkalmarrh <hentotheserpent@hotmail.co.uk>
2023-07-22 13:52:34 +0100

    "Outputs mangled hiragana for UTAU"

--- mangler.py
+++ mangler.py
@@ -4,10 +4,14 @@ import sys
 from collections.abc import Generator
 import re
 import pronouncing, cmudict
+from wanakana import to_hiragana, is_romaji, is_hiragana
+
+translator = googletrans.Translator()
 
 def random_lang(blacklist: list) -> str:
+    blacklist.extend(["en", "eo"])
     langs = [lang for lang in googletrans.LANGUAGES.keys() if 
-             lang not in blacklist and lang !="en" and lang != "eo"]
+             lang not in blacklist]
     return random.choice(langs)
 
 def parse_txt(file: str) -> list:
@@ -18,31 +22,31 @@ def get_words(line) -> Generator[str]:
     regex = re.compile('[,\.!?]')
     yield from [regex.sub('', word).strip() for word in line.split(" ")]
 
-def mangler(word: str, translator: googletrans.Translator, 
+def random_translate(word: str, 
             exhausted_langs = None) -> str:
     exhausted_langs = [] if not exhausted_langs else exhausted_langs
     lang = random_lang(blacklist = exhausted_langs)
-    mangled_word = translator.translate(word, dest = lang).pronunciation
-    if mangled_word:
-        return mangled_word
+    trans_word = translator.translate(word, src = "en", dest = lang).pronunciation
+    if trans_word:
+        return trans_word
     else:
         exhausted_langs.append(lang)
-        return mangler(word, translator, exhausted_langs)
+        return random_translate(word, exhausted_langs)
 
-def get_syllables(word):
+def get_syllables(word: str) -> int:
     phones = pronouncing.phones_for_word(word)
     return sum([pronouncing.syllable_count(p) for p in phones])
 
-def pronounce_natural(phones: list) -> str:
+def pronounce_natural(phones: list) -> Generator[str]:
     phones_str = "".join(phones) 
-    return re.sub(r'\d+', '', phones_str)
+    yield re.sub(r'\d+', '', phones_str)
 
-def trunc_word(phones: list, target_sylls: int) -> str:
+def trunc_word(phones: list, target_sylls: int) -> Generator[str]:
     slice_num = target_sylls - 1
-    return phones[:slice_num]
+    yield from phones[:slice_num]
     
 def match_syllables(before: list, after: list) -> str:
-    def syll_count(phones):
+    def syll_count(phones: list) -> int:
         return sum([pronouncing.syllable_count(p) for p in phones])   
     cleaned_line = []
     for b, a in zip(before, after):
@@ -53,33 +57,41 @@ def match_syllables(before: list, after: list) -> str:
             a_phones = trunc_word(a_phones, b_count)
         cleaned_line.append(pronounce_natural(a_phones))
     return " ".join(cleaned_line)
-                
-def get_mangled_lines(file: str) -> Generator[list]:
+               
+def get_mangled_lines(file: str) -> Generator[str]:
     lines = parse_txt(file)
-    translator = googletrans.Translator()
-    mangled_lines = []
     for line in lines:
-        mangled_line = [mangler(word, translator) for word in get_words(line)]
-        mangled_lines.append(mangled_line)
-    return lines, mangled_lines
+        if line.strip():
+            yield " ".join([random_translate(word) for word in get_words(line)])
 
 def dump_new_lines(lines: list, outfile: str):
+    for i, line in enumerate(lines):
+        if not line.endswith("\n"):
+            lines[i] = line + "\n"
     with open(outfile, "w+", encoding = "utf-8") as txt:
         txt.writelines(lines)
-        
+            
+def mangled_to_hiragana(line: str) -> Generator[str]:
+    en_line = [translator.translate(word, dest = "en").text 
+               for word in line.split()]
+    for word in en_line:
+        if is_romaji(word):
+            yield clean_hiragana(to_hiragana(word))
+        else:
+            continue   
+
+def clean_hiragana(word: str) -> str:
+    return "".join([letter for letter in word if is_hiragana(letter)])
+    
 def main():
     file = sys.argv[1]
     outfile = sys.argv[2]
-    befores, afters = get_mangled_lines(file)
-    cleaned_lines = []
-    #The cmudict isn't able to guess phonemes from non-English words.
-    #for before, after in zip(befores, afters):
-    #    cleaned_lines.append(match_syllables(before.split(), after) + "\n")
-    for line in afters:
-        cleaned_lines.append(" ".join(line))
-    dump_new_lines(cleaned_lines, outfile)
+    mangled_lines = [line for line in get_mangled_lines(file)]
+    dump_new_lines(mangled_lines, outfile)
+    jp_lines = []
+    for line in mangled_lines:
+        jp_lines.append(" ".join([word for word in mangled_to_hiragana(line)]))
+    dump_new_lines(jp_lines, "jp_dump.txt")
      
 if __name__ == "__main__":
     sys.exit(main())
-    
-
